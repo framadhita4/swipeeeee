7d18b231e8db6df0b37d5049f665fd6047556d9d
Framadhita <framadhita@outlook.com>
2026-02-27 19:59:12 +0700

    Add @netlify/vite-plugin to example project and update Vite configuration.

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import glsl from 'vite-plugin-glsl'
 import glslify from './vite-plugin-glslify'
+import netlify from '@netlify/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), glslify(), glsl()],
+  plugins: [react(), glslify(), glsl(), netlify()],
 })
